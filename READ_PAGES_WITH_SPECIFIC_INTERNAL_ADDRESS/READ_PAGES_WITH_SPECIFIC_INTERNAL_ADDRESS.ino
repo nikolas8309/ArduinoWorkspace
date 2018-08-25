c964254148cb633bea8788c927bab33a3a6c8b85
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2018-08-25 21:48:24 +0300

    proodos me to tsipaki pithanon 64 byte page

---
+++ READ_PAGES_WITH_SPECIFIC_INTERNAL_ADDRESS/READ_PAGES_WITH_SPECIFIC_INTERNAL_ADDRESS.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+
+#define READBYTES 364
+
+uint8_t inBuffer[READBYTES];
+
+void setup() {
+  
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  delay(5000);
+  
+  Wire.beginTransmission (0x68); 
+
+  Wire.write (0x33);
+  //Wire.write (0x02);
+
+  Wire.endTransmission ();
+  
+  delay(50);
+}
+
+void loop() {
+  
+ Serial.println("LOOP");
+  Wire.requestFrom(0x68, READBYTES );    // request READBYTES bytes from slave device 0x68
+
+  //while(Wire.available())    // slave may send less than requested
+  for(int j=0;j<READBYTES ;j++)
+  { 
+    //char c = Wire.read(); // receive a byte as character
+    inBuffer[j]=Wire.read();
+   // Serial.print(inBuffer[j]);         // print the character
+  }
+  
+  Serial.println();
+  Serial.println("AS HEX[]");
+  for(int j=0;j<READBYTES;j++){ 
+    Serial.print(inBuffer[j],HEX);
+    Serial.print(" ");         
+  }
+
+//  Serial.println();
+//  Serial.println("AS CHAR[]");
+//  for(int j=0;j<READBYTES;j++){ 
+//    Serial.print((char)inBuffer[j]);
+//  }
+  
+  Serial.println();
+  Serial.println("DONE");
+
+  delay(2000);
+
+}
